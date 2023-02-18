41d3e7dea2bcef67bca21f8f4b135333996f12bf
Mokhtar Zehrawi <mokhtar.zehrawi@gmail.com>
2023-02-18 18:45:07 -0500

    push1

--- Mokhtar_Zehrawi_CV_Web1/Mokhtar_Zehrawi_CV_Web1/Code.js
+++ Mokhtar_Zehrawi_CV_Web1/Mokhtar_Zehrawi_CV_Web1/Code.js
@@ -1,1 +1,36 @@
-﻿
+﻿console.log('hello world');
+
+var a123 = 'hello world';
+//console.log(document.getElementsByName('firstname').length);
+function buttonClick() {
+    var FirstName = document.getElementById('n1');
+    var LastName = document.getElementById('n2');
+    var Email = document.getElementById('n3');
+
+
+    if (FirstName.value.trim() === "") {
+
+        alert('fill out first name please');
+        event.preventDefault(); //prevents form submission
+
+    } else if (LastName.value.trim() === "") {
+
+        alert('fill out last name please');
+        event.preventDefault(); //prevents form submission
+
+    } else if (Email.value.trim() === "") {
+
+        alert('fill out email please');
+        event.preventDefault(); //prevents form submission
+
+    } else {
+
+        alert('Thank You')
+    }
+
+
+    
+}
+
+
+
